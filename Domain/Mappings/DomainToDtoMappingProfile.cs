852b3f9ef640f0b64215760588da522300d08646
Felipe Lopes Zem <felipelopeszem@hotmail.com>
2024-03-08 16:41:46 -0300

    empty projects, done some classes at Domain

---
+++ Domain/Mappings/DomainToDtoMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Mappings
+{
+    public class DomainToDtoMappingProfile : Profile
+    {
+        public DomainToDtoMappingProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
+    }
+}
---
+++ Domain/Services/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Domain.Dtos;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllProductsAsync()
+        {
+            var result = await _productRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ProductDto>>(result);
+        }
+
+        public async Task<ProductDto?> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> CreateProductAsync(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UpdateProductAsync(ProductDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
